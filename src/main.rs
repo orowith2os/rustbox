44f568718ffa768d770fa03d94d37da947b3b901
orowith2os <93224879+orowith2os@users.noreply.github.com>
2022-08-13 20:28:19 -0500

    Gets a working version subcommand, adds clap

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-//mod create;
-//mod enter;
-//mod list;
-//mod stop;
-//mod rm;
-//mod ephemeral;
+use clap::Parser;
+//use std::env::args;
+
 const VERSION: i32 = 0; //change this on version updates
 
+#[derive(Parser, Debug)]
+#[clap(version, long_about = None)]
+struct Args {
+    /// Print Rustbox's version
+    #[clap(long = "version", value_parser)]
+    version: bool,
+}
+
 fn main() {
-    let availablecommands = "create
+    let args = Args::parse();
+
+    if args.version == true {
+        println!("Rustbox version: {VERSION}");
+    }
+}
+
+fn noarguments() {
+    let availablecommands = 
+       "create
         enter
         list
         stop
@@ -15,7 +29,8 @@ fn main() {
         version";
     println!("Error: invalid command");
     println!("Rustbox version: {VERSION}");
-    println!("Choose one of the available commands: \n 
-        {availablecommands}");
- 
+    println!(
+        "Choose one of the available commands: \n 
+        {availablecommands}"
+    );
 }
