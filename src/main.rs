d75d6780bb954674dec43247af87f715d4b6cfa3
orowith2os <93224879+orowith2os@users.noreply.github.com>
2022-08-22 22:21:50 -0500

    Get all of the arguments added

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,73 @@
 pub(crate) use clap::Parser;
+
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[derive(Parser, Debug)]
 #[clap(version, long_about = None)]
 struct Args {
-    /// Print Rustbox's version
-    #[clap(short = 'v', long = "version", value_parser)]
+    /// Print Rustbox's version.
+    #[clap(short = 'v', long = "version")]
     version: bool,
+
+    /// Enter a container.
+    #[clap(long = "enter")]
+    enter: bool,
+
+    /// List all containers.
+    #[clap(short = 'l', long = "list")]
+    list: bool,
+
+    /// Stop a container, or all containers.
+    #[clap(long = "stop")]
+    stop: bool,
+
+    /// Remove a container.
+    #[clap(long = "remove")]
+    remove: bool,
+
+    /// Make a temporary container.
+    #[clap(long = "ephemeral")]
+    ephemeral: bool,
 }
+
 fn main() {
     let args = Args::parse();
+
     if args.version {}
     {
         println!("Rustbox version: {VERSION}");
     }
+    if args.enter {}
+    {
+        println!("Enter a container. Not yet implemented.")
+    }
+    if args.list {}
+    {
+        println!("List all containers. Not yet implemented.")
+    }
+    if args.stop {}
+    {
+        println!("Stop a container, or all containers. Not yet implemented.")
+    }
+    if args.remove {}
+    {
+        println!("Remove a container. Not yet implemented.")
+    }
+    if args.ephemeral {}
+    {
+        println!(
+            "Make a temporary container that lasts for until you exit it. Not yet implemented."
+        )
+    }
 }
-
 
 // fn noarguments() {
 //     let availablecommands = "create
-//         enter
-//         list
-//         stop
 //         rm
-//         version";
 //     println!("Error: invalid command");
 //     println!("Rustbox version: {VERSION}");
 //     println!(
-//         "Choose one of the available commands: \n 
+//         "Choose one of the available commands: \n
 //         {availablecommands}"
 //     );
 // } I've commented these lines out, as Clap's built-in --help is good enough.
